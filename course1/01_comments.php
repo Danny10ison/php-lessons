cd8e53e295a8bb7f6b270b8220a7a4fa0bb86a1e
Danny10ison <pagesngod@gmail.com>
2023-08-08 17:53:22 +0000

    Add comments file
    Shows the various ways to commnt in php and documentation

---
+++ course1/01_comments.php
@@ -0,0 +1,15 @@
+<?php
+    // there are 3 ways to declare comments
+
+    // Single line comment
+    # Single line comment
+
+    /* Multi
+       Line
+       Comment
+    */
+
+    /**
+     * The is a documentation 
+     * comment
+     */
