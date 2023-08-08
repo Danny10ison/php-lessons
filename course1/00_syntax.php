517fc4d38ada0cf97b0de1154f410617d4f34af4
Danny10ison <pagesngod@gmail.com>
2023-08-08 17:52:26 +0000

    Add basic php syntax file

---
+++ course1/00_syntax.php
@@ -0,0 +1,26 @@
+<!-- Basic php syntax -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        echo "Hello world";
+        echo " and PHP";
+    ?>
+
+    <?php
+        echo "From another tag";
+    ?>
+    <!-- you can echo html tag -->
+
+    <?php
+        echo '<br>';
+        echo '<button>Click me</button>';
+    ?>
+
+</body>
+</html>
